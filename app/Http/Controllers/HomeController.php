8dc0452e3c0b7fa34f2e790054d5713b4005f891
ShadowDark93 <davidortegacadena@gmail.com>
2021-09-30 08:18:04 -0500

    commit-1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('pacientes.index');
+        return redirect()->route('pacientes.index');
     }
 }
--- resources/views/pacientes/index.blade.php
+++ resources/views/pacientes/index.blade.php
@@ -1,1 +1,36 @@
 @extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Cédula</th>
+                    <th>Nombres</th>
+                    <th>Apellidos</th>
+                    <th>Genero</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($pacientes as $paciente)
+                    <tr>
+                        <td scope="row">{{ $paciente->numero_documento }}</td>
+                        <td>{{ $paciente->nombre1 }} {{ $paciente->nombre2 }}</td>
+                        <td>{{ $paciente->apellido1 }} {{ $paciente->apellido2 }}</td>
+                        <td>
+                            @if ($paciente->genero_id == 1)
+                                Masculino
+                            @else
+                                Femenino
+                            @endif
+                        </td>
+                        <td>
+                            <col-span><a class="btn btn-primary">Editar</a></col-span>
+                            <col-span><a class="btn btn-warning">Eliminar</a></col-span>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
